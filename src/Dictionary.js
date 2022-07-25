bb98692e6264a699655c817250228c5a5815fdeb
meera-raveendran <100879326+meera-raveendran@users.noreply.github.com>
2022-07-24 23:50:01 -0500

    added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,15 @@
 import React ,{useState}from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary(){
     let [keyword,setKeyword]=useState("");
+    function handleResponse(response){
+        console.log(response.data[0])
+    }
     function Search(event){
-        event.preventDefault();
-        alert(`searching for ${keyword} definition`);
+        event.preventDefault();        
+        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
     function handleKeywordChange(event){
         setKeyword(event.target.value);
